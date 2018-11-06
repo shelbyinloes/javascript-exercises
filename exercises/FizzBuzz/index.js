d0a636ff2b73341fc6656603742563b48be95f6a
Shelby Inloes <shelbyinloes@gmail.com>
2018-11-05 21:32:31 -0600

    fizzbuzz + json

--- exercises/FizzBuzz/index.js
+++ exercises/FizzBuzz/index.js
@@ -17,7 +17,18 @@
  */
 
 function fizzBuzz(n) {
-
+//for loop
+    for(i = 1; i <= n; i++){
+        if((i % 5 === 0) && (i % 3 === 0)){
+            console.log('fizzbuzz');
+        }else if(i % 5 === 0){
+            console.log('buzz')
+        }else if(i % 3 === 0){
+            console.log('fizz')
+        }else{
+            console.log(i)
+        }
+    }
 }
 
 module.exports = fizzBuzz;
