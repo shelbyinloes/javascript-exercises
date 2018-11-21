4ee9e508bd353b0d8cb01c6dd6972ad90d1fe4e9
Shelby Inloes <shelbyinloes@gmail.com>
2018-11-20 22:03:23 -0600

    brainstorming

--- exercises/MatrixSpiral/index.js
+++ exercises/MatrixSpiral/index.js
@@ -19,6 +19,13 @@
  */
 
 function matrix(n) {
+    //thought process:
+    //whatever the number is for the matrix, 
+    // I will need that many arrays + 
+    // that many items in each array
+
+    //first array push increment to array until length is hit
+    //okay dont know from here, will research
 
 }
 
