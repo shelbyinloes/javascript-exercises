e1bca975a289cfdf788792b4c1375886a939748d
Shelby Inloes <shelbyinloes@gmail.com>
2019-01-09 19:36:19 -0600

    finished

--- exercises/ReverseInteger/index.js
+++ exercises/ReverseInteger/index.js
@@ -12,8 +12,32 @@
  * reverseInt(-90) === -9
  */
 
-function reverse(int) {
+function reverse(num) {
+    let negative;
+  // putting into an array
+  const numArr = num.toString().split('')
+  
+  // checking for negative
+  if(numArr[0] == '-') {
+    console.log('negativo')
+    numArr.shift();
+    negative = true;
+    console.log(negative)
+  }
 
+  // while the last number in the array is 0, pop off
+  while(numArr[numArr.length-1] == 0){
+    numArr.pop();
+  }
+
+  //adding back in the negative
+  if(negative == true){
+    console.log("still here")
+    numArr.push('-');
+  }
+  //put it all back together
+  const numStr =  numArr.reverse().join('');
+  return Number(numStr);
 }
 
 module.exports = reverse;
